18494910e315b9da7e9867ed47749c75ef605e56
Rostislav Mykhajliw <necromant2005@gmail.com>
2013-09-17 09:39:29 +0300

    update classmap

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -3,4 +3,8 @@ return array(
     'BigMl\Client\BigMl' => __DIR__ . '/src/BigMl/Client/BigMl.php',
     'BigMl\Resource\AbstractResource' => __DIR__ . '/src/BigMl/Resource/AbstractResource.php',
     'BigMl\Resource\Source' => __DIR__ . '/src/BigMl/Resource/Source.php',
+    'BigMl\Resource\Dataset' => __DIR__ . '/src/BigMl/Resource/Dataset.php',
+    'BigMl\Resource\Model' => __DIR__ . '/src/BigMl/Resource/Model.php',
+    'BigMl\Resource\Prediction' => __DIR__ . '/src/BigMl/Resource/Prediction.php',
+    'BigMl\Resource\Evaluation' => __DIR__ . '/src/BigMl/Resource/Evaluation.php',
 );
